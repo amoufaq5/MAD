0329ffd7c21cdc63e4d6e4ead82c5d61bc33b9eb
amoufaq5 <137143672+amoufaq5@users.noreply.github.com>
2025-03-22 08:50:53 +0200

    Update multimodal_model.py

--- models/multimodal_model.py
+++ models/multimodal_model.py
@@ -1,16 +1,16 @@
-# File: models/multimodal_model.py
+# models/multimodal_model.py
 import tensorflow as tf
 from tensorflow.keras import layers, Model
 
-def build_text_branch(vocab_size=10000, embedding_dim=128, max_length=100):
+def build_text_branch(vocab_size=5000, embedding_dim=128, max_length=100):
     text_input = layers.Input(shape=(max_length,), name="text_input")
     x = layers.Embedding(input_dim=vocab_size, output_dim=embedding_dim, input_length=max_length)(text_input)
     x = layers.LSTM(64)(x)
     x = layers.Dense(32, activation="relu")(x)
     return text_input, x
 
-def build_image_branch(input_shape=(224, 224, 3)):
-    image_input = layers.Input(shape=input_shape, name="image_input")
+def build_image_branch(image_shape=(224,224,3)):
+    image_input = layers.Input(shape=image_shape, name="image_input")
     x = layers.Conv2D(32, (3,3), activation="relu")(image_input)
     x = layers.MaxPooling2D()(x)
     x = layers.Conv2D(64, (3,3), activation="relu")(x)
@@ -25,20 +25,15 @@ def build_blood_branch(input_dim=10):
     x = layers.Dense(16, activation="relu")(x)
     return blood_input, x
 
-def build_multimodal_model(num_diseases=10):
+def build_multimodal_model():
     text_input, text_out = build_text_branch()
     image_input, image_out = build_image_branch()
     blood_input, blood_out = build_blood_branch()
 
+    # Combine
     combined = layers.concatenate([text_out, image_out, blood_out])
     x = layers.Dense(64, activation="relu")(combined)
-    # Output: classification among 10 diseases
-    output = layers.Dense(num_diseases, activation="softmax", name="diagnosis")(x)
-
+    # We assume 9 diseases: cold, pneumonia, bronchitis, influenza, herpes, covid, hep B, chlamydia, chicken pox
+    output = layers.Dense(9, activation="softmax", name="diagnosis")(x)
     model = Model(inputs=[text_input, image_input, blood_input], outputs=output)
     return model
-
-if __name__ == "__main__":
-    model = build_multimodal_model()
-    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
-    model.summary()
